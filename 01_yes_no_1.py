0835ddb0e8a93c394d7fbd0a0172cab4cbe4cf35
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-04-14 10:23:38 +1200

    Simplified code

--- 01_yes_no_1.py
+++ 01_yes_no_1.py
@@ -1,22 +1,19 @@
 looping = True
 
 while looping:
-    show_instructions = input("Have you played Lucky Unicorn before? ")
+    show_instructions = input("Have you played Lucky Unicorn before? ").lower()
 
     if show_instructions == "yes":
         print("<<<CONTINUE TO GAME>>>")
 
-    if show_instructions == "y":
+    elif show_instructions == "y":
         print("<<<CONTINUE TO GAME>>>")
 
-    if show_instructions == "no":
+    elif show_instructions == "no":
         print("<<<INSTRUCTIONS>>>")
 
-    if show_instructions == "n":
+    elif show_instructions == "n":
         print("<<<INSTRUCTIONS>>>")
-
-    if show_instructions == "end":
-        looping = False
 
     else:
         print("<<<ERROR>>>")
