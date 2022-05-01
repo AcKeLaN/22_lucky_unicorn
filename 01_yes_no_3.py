9da063c6d430715faa640887684c7a03e728be52
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-05-02 10:37:19 +1200

    Changed main routine

--- 01_yes_no_3.py
+++ 01_yes_no_3.py
@@ -1,26 +1,26 @@
 def yes_no_checker(question):
 
-    # Ask the user if they have played before
-    user_response = input(question).strip().lower()
+    looping = True
 
-    # If user answers yes, game continues
-    if user_response == "yes" or user_response == "y":
-        user_response = "yes"
-        return user_response
+    while looping:
+        # Ask the user if they have played before
+        user_response = input(question).strip().lower()
 
-    # If the user answers no, display the instructions
-    elif user_response == "no" or user_response == "n":
-        user_response = "no"
-        return user_response
+        # If user answers yes, game continues
+        if user_response == "yes" or user_response == "y":
+            user_response = "yes"
+            return user_response
 
-    # If user answers anything other than yes or no
-    else:
-        print("<<<ERROR>>>\n<<<PLEASE ENTER YES OR NO>>>")
+        # If the user answers no, display the instructions
+        elif user_response == "no" or user_response == "n":
+            user_response = "no"
+            return user_response
+
+        # If user answers anything other than yes or no
+        else:
+            print("<<<ERROR>>>\n<<<PLEASE ENTER YES OR NO>>>")
 
 
 # Main routine...
-looping = True
-
-while looping:
-    show_instructions = yes_no_checker("Have you played Lucky Unicorn before? ")
-    print(f"You chose {show_instructions}")
+show_instructions = yes_no_checker("Have you played Lucky Unicorn before? ")
+print(f"You chose {show_instructions}")
