67ed7f918aac4b62374eb45e94eb45f108e15e90
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-05-05 15:07:03 +1200

    Two 'versions' of my code

---
+++ 03_number_checker_1.py
@@ -0,0 +1,48 @@
+# Yes/No Checker function for checking validity of inputted numbers
+def yes_no_checker(question):
+
+    looping = True
+
+    while looping:
+        # Ask the user if they have played before
+        user_response = input(question).strip().lower()
+
+        # If user answers yes, game continues
+        if user_response == "yes" or user_response == "y":
+            user_response = "yes"
+            return user_response
+
+        # If the user answers no, display the instructions
+        elif user_response == "no" or user_response == "n":
+            user_response = "no"
+            return user_response
+
+        # If user answers anything other than yes or no
+        else:
+            print("<<<ERROR>>>\n<<<PLEASE ENTER YES OR NO>>>")
+
+
+def number_checker(question):
+
+    looping = True
+
+    while looping:
+
+        try:
+            bet_amount = int(input(question))
+
+            if 0 < bet_amount < 11:
+                recheck_bet_amount = yes_no_checker(f"Are you sure you would like to bet ${bet_amount}? ")
+                if recheck_bet_amount == "yes":
+                    return bet_amount
+
+            else:
+                print("Please enter a number between 1 and 10.")
+
+        except ValueError:
+            print("Please enter a number between 1 and 10.")
+
+
+# Main Routine
+bet_counter = number_checker("How much money would you like to bet on your game? ")
+print(f"You are betting ${bet_counter}")
