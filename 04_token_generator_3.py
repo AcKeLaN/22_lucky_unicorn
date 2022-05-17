0938c9e7bbbc9ccb02b98f13af74033af451eb60
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-05-17 12:21:41 +1200

    Added variable names

--- 04_token_generator_3.py
+++ 04_token_generator_3.py
@@ -1,24 +1,23 @@
 import random
 
 # Main Routine
-tokens = ["unicorn", "zebra", "zebra", "zebra",
-          "horse", "horse", "horse",
-          "donkey", "donkey", "donkey"]
-
 STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
 
 # Testing loop to generate 20 tokens
-for i in range(0, 500):
-    chosen = random.choice(tokens)
+for i in range(0, 100):
+    chosen_num = random.randint(1, 100)
 
     # Adjust balance
-    if chosen == "unicorn":
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
         balance += 4
-    elif chosen == "donkey":
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
         balance -= 1
     else:
+        chosen = "horse / zebra"
         balance -= 0.5
 
     # Output
