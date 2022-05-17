9b816cfd1a21cc91baaf412e2929455da2af33ab
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-05-17 12:11:44 +1200

    Retried creating a token_generator function, with a different format

--- 04_token_generator_3.py
+++ 04_token_generator_3.py
@@ -1,11 +1,16 @@
 import random
 
 # Main Routine
-tokens = ["unicorn", "zebra", "horse", "donkey"]
-balance = 100
+tokens = ["unicorn", "zebra", "zebra", "zebra",
+          "horse", "horse", "horse",
+          "donkey", "donkey", "donkey"]
+
+STARTING_BALANCE = 100
+
+balance = STARTING_BALANCE
 
 # Testing loop to generate 20 tokens
-for i in range(0, 20):
+for i in range(0, 500):
     chosen = random.choice(tokens)
 
     # Adjust balance
@@ -18,3 +23,6 @@ for i in range(0, 20):
 
     # Output
     print(f"Token: {chosen} / Balance: ${balance}")
+
+print(f"Starting Balance: ${STARTING_BALANCE:.2f}")
+print(f"Final Balance: ${balance:.2f}")
