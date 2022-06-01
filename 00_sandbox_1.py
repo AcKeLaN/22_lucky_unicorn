0bf4dfa927b1e614c61b05f9d2d95fc0b08502c5
AcKeLaN <mace.ciara@awatapu.school.nz>
2022-06-01 13:49:33 +1200

    sandbox lol

--- 00_sandbox_1.py
+++ 00_sandbox_1.py
@@ -4,7 +4,7 @@ for i in range(0, 20):
     number = random.randint(0, 4)
     print(number, end='\n')
 
-greeting = "hello world"
+greeting = "GO FUCK YOURSELF"
 sides = "*" * 3
 
 greeting = f"{sides}  {greeting}  {sides}"
